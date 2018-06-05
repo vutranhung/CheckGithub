6788922bfe3bdbf7b26fcb596e8f22cecdceffc1
Thinkpad <Thinkpad@THINK>
2018-06-05 09:51:38 +0700

    Add project files.

---
+++ CheckGithub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheckGithub.Startup))]
+namespace CheckGithub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
